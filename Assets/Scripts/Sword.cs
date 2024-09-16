44241819ca27b0320888245230d226f4fb28c79a
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-09-16 17:05:03 +0700

    etou

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+public class Sword : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+    }
+}
