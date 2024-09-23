fe0e7111f9bd0e388940e7ae27043a481970fc40
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-09-23 17:18:05 +0700

    .

---
+++ Assets/Scripts/TransparentDetection.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TransparentDetection : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] private float transparencyAmount = 0.8f;
+    [SerializeField] private float fadeTime = 0.4f;
+
+    private SpriteRenderer spriteRenderer;
+    private Tilemap tilemap;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>())
+        {
+            if (spriteRenderer)
+            {
+                StartCoroutine(FadeRout)
+            }else if(tilemap){
+
+            }
+        }
+    }
+    private IEnumerator FadeRoutine(SpriteRenderer spriteRenderer, float fadeTime, float startValue, float targetTransparency)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < fadeTime) { 
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, targetTransparency, elapsedTime/fadeTime);
+            spriteRenderer.color = new Color()
+        }
+}
