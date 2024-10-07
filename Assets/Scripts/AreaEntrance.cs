a0c801d8df21a6b4888d59196c75041d06966c5e
OtakuIsMe <duynmse173649@fpt.edu.vn>
2024-10-07 14:30:27 +0700

    .

---
+++ Assets/Scripts/AreaEntrance.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEntrance : MonoBehaviour
+{
+    [SerializeField] private string transitionName;
+
+    private void Start()
+    {
+        if (transitionName == SceneMangement.Instance.SceneTransitionName)
+        {
+            PlayerController.Instance.transform.position = this.transform.position;
+            UIFade.Instance.FadeToClear();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneMangement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMangement : Singleton<SceneMangement>
+{
+    public string SceneTransitionName { get; private set; }
+
+    public void SetTransitionName(string sceneTransitionName)
+    {
+        this.SceneTransitionName = sceneTransitionName;
+    }
+}
